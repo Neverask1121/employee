3280e977f22c3674fa695a8b829d88e5d6dcbf92
aditya bhandari <20252501002@iiitvadodara.ac.in>
2026-01-01 13:03:13 +0530

    fixes for render

--- app.py
+++ app.py
@@ -72,3 +72,11 @@ def index():
     conn.close()
 
     return render_template("index.html", entries=database)  # this was corrected
+
+
+    #required for render
+    if __name__ == "__main__":
+        app.run(
+            host="0.0.0.0",
+            port=int(os.environ.get("PORT", 5000))
+        )
